c319cd6c604b90915c8cce5f4f00edac9afb3b0e
grzs <gerzsonj@gmail.com>
2020-11-28 01:02:38 +0000

    new indentation

--- ansible-role-explorer.sh
+++ ansible-role-explorer.sh
@@ -7,15 +7,25 @@ if [[ ! -f "${1}/${root}/${start}" ]]; then
     exit 1
 fi
 
-# initial values for function
+# initial values for functions
 steps=0
 route=()
-indent=""
-indent_str="|     "
+tab=4
+level=0
+task_prefix=""
 
-# function definition
+# function definitions
+function indenter {
+    echo
+    i=0; while [[ i -ne $level ]]; do
+	printf "|%${tab}s"
+	let i++
+    done
+}
+
 function walk {
-    echo "${indent}>> ${1}"
+    indenter
+    printf "%s %s" "\\" "${1}"
 
     # append path to route
     route=( ${route[@]} ${1} )
@@ -27,32 +37,45 @@ function walk {
     for l in ${lines[@]}; do
 	case $l in
 	    -\ name:*)
-		let "steps++"
-		printf "%s%03d %s\n" "$indent" "$steps" "${l}"
+		if [[ $task_cur ]]; then
+		    indenter
+		    printf "%s %s" "$task_prefix" "${task_cur}"
+		fi
+		let steps++
+		task_prefix=`printf "%03d" ${steps}`
+		task_cur=$(echo $l | sed 's/^- name: //')
 		;;
-	    block:x*)
-		echo -n "${indent}${indent_str}"
-		echo ${l}
+	    block:*)
+		let steps--
+		task_prefix="  { block start -"
+		;;
+	    rescue:*)
+		let steps--
+		task_prefix="  } rescue :"
 		;;
 	    when:x*)
 		echo -n "${indent}${indent_str}"
 		echo ${l}
 		echo "${indent}${indent_str}"
 		;;
-	    include_tasks:*)
+	    include_tasks*|import_tasks*)
 		included=$(echo $l | awk '{print $2}')
-		echo -n "${indent}${indent_str}"
-		echo -e "include_tasks:"
-		echo "${indent}${indent_str}${indent_str}"
+		let steps--
+		task_prefix="[include_tasks]"
+		
+		indenter
+		printf "%s" "|\_"
+		indenter
+		printf "%s %s" "|  \_  ${task_prefix} -" "${task_cur}"
+		unset task_cur
 
 		# check if relative include and set parent dir
 		parent_dir=`dirname ${route[-1]}`
 		[[ -f "${parent_dir}/${included}" ]] || parent_dir="${root}"
 
 		# dive into included
-		indent="${indent}${indent_str}"
+		let "level++"
 		walk "${parent_dir}/${included}"
-		indent="${indent:: -${#indent_str}}"
 		# resume from included
 		;;
 	    *)
@@ -61,17 +84,26 @@ function walk {
 	esac
     done
     # reached EOF
+    # print last task
+    if [[ $task_cur ]]; then
+	indenter
+	printf "%s %s" "$task_prefix" "${task_cur}"
+    fi
+	unset task_cur
+
     # pop path from route
     unset 'route[${#route[@]}-1]'
     if [[ ${#route[@]} > 0 ]]; then
-	echo "${indent}"
-	echo "${indent::-6}<< ${route[-1]}"
+	indenter
+	let "level--"
+	indenter
+	printf "%s %s" "/" "${route[-1]}"
     fi
 }
 
 cd $1
 role=`basename ${PWD}`
-echo -e "Tasks in role ${role}:\n"
+echo -e "Tasks in role ${role}:"
 
 walk "${root}/${start}"
 
